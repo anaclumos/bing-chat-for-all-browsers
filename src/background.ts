85cbcdb42e254e28eb74e5d3ad98463a01bef915
Sunghyun Cho <mail@chosunghyun.com>
2023-02-10 18:54:35 -0800

    [wip] implement UA switcher

--- src/background.ts
+++ src/background.ts
@@ -1,6 +1,23 @@
-function polling() {
-  // console.log("polling");
-  setTimeout(polling, 1000 * 30);
-}
+import BlockingResponse = chrome.webRequest.BlockingResponse
+import WebRequestHeadersDetails = chrome.webRequest.WebRequestHeadersDetails
 
-polling();
+chrome.webRequest.onBeforeSendHeaders.addListener(
+  (details: WebRequestHeadersDetails): BlockingResponse | void => {
+    const { requestHeaders } = details
+    if (requestHeaders) {
+      const newHeaders = requestHeaders.map((header) => {
+        if (header.name.toLowerCase() === 'user-agent') {
+          console.log('Changing user agent from', header.value)
+          header.value = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36 Edg/110.0.100.0'
+          console.log('Changing user agent to', header.value)
+        }
+        return header
+      })
+      return { requestHeaders: newHeaders }
+    }
+    return undefined
+  },
+  { urls: ['*://*.bing.com/*'] },
+  ['blocking', 'requestHeaders'],
+)
+
--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -5,10 +5,7 @@ const srcDir = path.join(__dirname, "..", "src");
 
 module.exports = {
     entry: {
-      popup: path.join(srcDir, 'popup.tsx'),
-      options: path.join(srcDir, 'options.tsx'),
-      background: path.join(srcDir, 'background.ts'),
-      content_script: path.join(srcDir, 'content_script.tsx'),
+        background: path.join(srcDir, 'background.ts'),
     },
     output: {
         path: path.join(__dirname, "../dist/js"),
@@ -18,7 +15,7 @@ module.exports = {
         splitChunks: {
             name: "vendor",
             chunks(chunk) {
-              return chunk.name !== 'background';
+                return chunk.name !== 'background';
             }
         },
     },
