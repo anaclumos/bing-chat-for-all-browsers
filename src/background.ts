d920ce6ac16cffa5b9d95584e24f662092d3a5fd
Sunghyun Cho <mail@chosunghyun.com>
2023-02-16 11:23:30 -0800

    refactor codes

--- src/background.ts
+++ src/background.ts
@@ -1,26 +1,22 @@
 import { browser } from 'webextension-polyfill-ts'
 
+const MOBILE_UA =
+  'Mozilla/5.0 (Linux; Android 13; Pixel 6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Mobile Safari/537.36 EdgA/110.0.1587.41'
+const DESKTOP_UA =
+  'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36 Edg/110.0.100.0'
+
 browser.webRequest.onBeforeSendHeaders.addListener(
   (details) => {
     const { requestHeaders } = details
-    if (requestHeaders) {
-      const newHeaders = requestHeaders.map((header) => {
-        if (header.name.toLowerCase() === 'user-agent') {
-          console.log('Changing user agent from', header.value)
-          if (header?.value?.includes('Mobile')) {
-            header.value =
-              'Mozilla/5.0 (Linux; Android 13; Pixel 6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Mobile Safari/537.36 EdgA/110.0.1587.41'
-          } else {
-            header.value =
-              'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36 Edg/110.0.100.0'
-          }
-          console.log('Changing user agent to', header.value)
-        }
-        return header
-      })
-      return { requestHeaders: newHeaders }
-    }
-    return undefined
+    if (!requestHeaders) return undefined
+    const newHeaders = requestHeaders.map((header) => {
+      if (header.name.toLowerCase() === 'user-agent') {
+        header.value = DESKTOP_UA
+        if (header.value?.toLowerCase().includes('mobile')) header.value = MOBILE_UA
+      }
+      return header
+    })
+    return { requestHeaders: newHeaders }
   },
   { urls: ['*://*.bing.com/*'] },
   ['blocking', 'requestHeaders']
@@ -28,6 +24,7 @@ browser.webRequest.onBeforeSendHeaders.addListener(
 
 browser.runtime.onInstalled.addListener((object) => {
   let externalUrl = 'http://github.com/anaclumos/bing-chat-for-all-browsers'
+  console.log(object.reason)
   if (object.reason.toLowerCase().includes('install')) {
     browser.tabs.create({ url: externalUrl })
   }
