d8881407690238044814f7ced1715fbd7526c22e
Sunghyun Cho <mail@chosunghyun.com>
2023-02-15 21:27:46 -0800

    Update user agent to detect if request is coming from mobile device.

--- src/background.ts
+++ src/background.ts
@@ -5,8 +5,13 @@ chrome.webRequest.onBeforeSendHeaders.addListener(
       const newHeaders = requestHeaders.map((header) => {
         if (header.name.toLowerCase() === 'user-agent') {
           console.log('Changing user agent from', header.value)
-          header.value =
-            'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36 Edg/110.0.100.0'
+          if (header?.value?.includes('Mobile')) {
+            header.value =
+              'Mozilla/5.0 (Linux; Android 13; Pixel 6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Mobile Safari/537.36 EdgA/110.0.1587.41'
+          } else {
+            header.value =
+              'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36 Edg/110.0.100.0'
+          }
           console.log('Changing user agent to', header.value)
         }
         return header
